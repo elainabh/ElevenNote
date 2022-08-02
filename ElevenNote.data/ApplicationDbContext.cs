228d848adac1bfc00fa5fc4753e1943e245c33f6
Elaina <elaina.hardebeck@gmail.com>
2022-08-02 19:21:48 -0400

    user table

--- ElevenNote.data/ApplicationDbContext.cs
+++ ElevenNote.data/ApplicationDbContext.cs
@@ -1,9 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using ElevenNote.data.Entities;
 
 namespace ElevenNote.data
 {
     public class ApplicationDbContext : DbContext
     {
         
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    public DbSet<UserEntity> Users { get; set; }
+
     }
+
 }
